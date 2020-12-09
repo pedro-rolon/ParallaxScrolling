d4c3914c1a4a9229692247302d7c3f63f8309cac
Pedro Rolon <pedro@MacBook-Pro-de-Pedro.local>
2020-12-09 00:22:04 -0300

    first commit

---
+++ ParallaxScrolling/ImageCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageCell.swift
+//  ParallaxScrolling
+//
+//  Created by Pedro Rolon on 2020-12-08.
+//
+
+import UIKit
+
+class ImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
